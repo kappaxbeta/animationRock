b8aaf4ae221110e1501a624c624d48f78425303f
Jens Bösche <jens.boesche@seitenbau.com>
2021-11-30 15:42:41 +0100

    add

--- src/App.js
+++ src/App.js
@@ -2,14 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 import {useRef, useState, useEffect} from "react"
 import styled, { keyframes,  css }  from "styled-components";
-import { gsap , } from "gsap";
+import { gsap  } from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
 import {headShake, shake, fadeInRightBig} from 'react-animations';
 import pink from './img/pink.png'
 import violet from './img/violet.png'
 import lime from './img/lime.png'
 import blue from './img/blue.png'
 import SplitText from './utils/SplitText3';
-import useLocoScroll from './utils/useLocoScroll'
+import useLocoScroll from './utils/useLocoScroll';
+import { Canvas, useFrame } from '@react-three/fiber';
+
+function Box(props) {
+    // This reference gives us direct access to the THREE.Mesh object
+    const ref = useRef()
+    // Hold state for hovered and clicked events
+    const [hovered, hover] = useState(false)
+    const [clicked, click] = useState(false)
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+    // Return the view, these are regular Threejs elements expressed in JSX
+    return (
+        <mesh
+            {...props}
+            ref={ref}
+            scale={clicked ? 1.5 : 1}
+            onClick={(event) => click(!clicked)}
+            onPointerOver={(event) => hover(true)}
+            onPointerOut={(event) => hover(false)}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    )
+}
+
 function App() {
     useLocoScroll();
 
@@ -18,7 +44,7 @@ function App() {
   min-height: 100vh;
   min-width: 100vw;
   color:white;
-  font-family: "Comic Sans MS";
+      position: relative;
 `;
 
     const Section = styled.div`
@@ -28,8 +54,10 @@ function App() {
   align-content: center;
   align-items: center;
 `;
+    const circle = useRef();
 
   useEffect(() => {
+      gsap.registerPlugin(ScrollTrigger);
      const split = new SplitText('.headline', {
           type: "lines",
           linesClass: "lineChildren"
@@ -47,9 +75,57 @@ function App() {
          stagger: 0.1,
          ease: 'power2'
      });
+
+     gsap.to(circle.current, {
+         scrollTrigger: circle.current,
+         delay: 2,
+         x: 0,
+         ease: 'power2'
+     });
+
+      let tl = gsap.timeline({
+          // yes, we can add it to an entire timeline!
+          scrollTrigger: {
+              trigger: "#main-container",
+              pin: true,   // pin the trigger element while active
+              start: "top top", // when the top of the trigger hits the top of the viewport
+              end: "bottom", // end after scrolling 500px beyond the start
+              scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+              snap: {
+                  snapTo: "h6", // snap to the closest label in the timeline
+                  duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+                  delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+                  ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
+              }
+          }
+      });
+
   },[]);
 
-  return (
+    function Box(props) {
+        // This reference gives us direct access to the THREE.Mesh object
+        const ref = useRef()
+        // Hold state for hovered and clicked events
+        const [hovered, hover] = useState(false)
+        const [clicked, click] = useState(false)
+        // Subscribe this component to the render-loop, rotate the mesh every frame
+        useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+        // Return the view, these are regular Threejs elements expressed in JSX
+        return (
+            <mesh
+                {...props}
+                ref={ref}
+                scale={clicked ? 1.5 : 1}
+                onClick={(event) => click(!clicked)}
+                onPointerOver={(event) => hover(true)}
+                onPointerOut={(event) => hover(false)}>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+            </mesh>
+        )
+    }
+
+    return (
    <main>
      <div >
          <Bg id={'main-container'} data-scroll-container>
@@ -64,13 +140,21 @@ function App() {
                      <img src={blue} data-scroll=""/>
                  </div>
              </Section>
-
-             <Section className={'featured-section'} data-scroll-section>
-                 <div className={'featured-row-layout'} >
+             <Section className={'featured-section-grow'} data-scroll-section>
+                 <div className={'featured-row-layout-grow'} >
                      <h6>GO beyond</h6>
-                     <img src={pink} data-scroll/>
+                     <img src={pink} width={300} height={300} style={{transform: 'translateX(calc(50vw + 150px))'}} ref={circle}data-scroll/>
                  </div>
              </Section>
+
+             <Section data-scroll-section>
+                 <Canvas height={"100vh"} data-scroll>
+                     <ambientLight />
+                     <pointLight position={[10, 10, 10]} />
+                     <Box position={[1.2, 0, 0]} />
+                 </Canvas>
+             </Section>
+
 
 
          </Bg>
